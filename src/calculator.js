70b5d708515e2d60baf6f438d7d3a9f939deccc4
Tristan Emmerson <tristan@stickerslug.com>
2020-06-26 13:08:07 -0700

    GREEN light ageMarsYears.

--- src/calculator.js
+++ src/calculator.js
@@ -7,10 +7,11 @@ let saturnDaysYear = 10759
 let uranusDaysYear = 30678
 let neptuneDaysYear = 60190
 
-export function Me (age, ageEarthYears, ageEarthDays, ageMercuryYears, ageVenusYears) {
+export function Me (age, ageEarthYears, ageEarthDays, ageMercuryYears, ageVenusYears, ageMarsYears) {
   this.age = age;
   this.ageEarthYears = age;
   this.ageEarthDays = ageEarthYears *  earthDaysYear;
   this.ageMercuryYears = Math.floor((ageEarthDays / mercuryDaysYear));
   this.ageVenusYears = Math.floor((ageEarthDays / venusDaysYear));
+  this.ageMarsYears = Math.floor((ageEarthDays / marsDaysYear));
 }
